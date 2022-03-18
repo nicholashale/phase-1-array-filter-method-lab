79caf393ce48f9ed32c77ca16a9161ffefb96bd9
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-03-17 17:50:20 -0700

    commit tree c8e188198b3711399af8ce0bb3a7d523c0ba8fee

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+const findMatching = (drivers, string) => drivers.filter(driverName => driverName.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (drivers, string) => drivers.filter(driverName => driverName.startsWith(string));
+
+const matchName = (drivers, string) => drivers.filter(driverObj => driverObj.name === string);
+
+
